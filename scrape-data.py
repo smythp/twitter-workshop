19bfb4c4bf0bda224854f6aa32c8b7e159ea881c
smythp <patricksmyth01@gmail.com>
2016-02-09 22:32:05 -0500

    Completed both scrape-date.py and simple-scrape.py. Still need to check for Python 2

--- scrape-data.py
+++ scrape-data.py
@@ -1,38 +1,17 @@
-import sys
 import tweepy
 import codecs
-from time import clock
 import my_keys
 
-# import time
-# from tweepy import Stream
-# from tweepy import OAuthHandler
-# from tweepy.streaming import StreamListener
-# import os
-
+# Get the authentication keys from my_keys.py
 CONSUMER_KEY = my_keys.CONSUMER_KEY
 CONSUMER_SECRET = my_keys.CONSUMER_SECRET
 ACCESS_KEY = my_keys.ACCESS_KEY
 ACCESS_SECRET = my_keys.ACCESS_SECRET
- 
 
+# Authenticate to Twitter with our keys
 auth1 = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth1.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth1)
-
-# class MyStreamListener(tweepy.StreamListener):
-#     def on_status(self, status):
-#         print(status.text)
-#     def on_error(self, status_code):
-#         if status_code == 420:
-#             return False
-
-            
-# myStreamListener = MyStreamListener()
-# print(type(myStreamListener))
-# myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-
-# print(myStream.filter(track=['#nycdh','#dh']))
 
 class StreamListener(tweepy.StreamListener):
     def on_status(self, status):
@@ -44,7 +23,6 @@ class StreamListener(tweepy.StreamListener):
         tweetid = status.id
 
         if not ('RT @' in tweet) :	
-
             print("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"" % (tweet,user,userid,time,source))
 
 StreamListener = StreamListener()
